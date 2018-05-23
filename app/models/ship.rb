289add1ec10f605196cea7ab05694146b38d078d
Vanessa Toro <vanetoro84@gmail.com>
2018-05-23 17:50:44 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,18 @@
 class Ship
+  attr_reader :name, :type, :booty
+
+    def initialize(name, type, booty)
+      @name = name
+      @type = type
+      @booty = booty
+      @@all<< self
+    end
+
+    def self.all
+      @@all  
+    end
+
+    def self.clear
+      @@all.clear
+    end  
 end
