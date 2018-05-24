ea3b3e7a2a700874d6908c6b656c1c8da499ab3a
Vanessa Toro <vanetoro84@gmail.com>
2018-05-24 15:33:12 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,10 +14,10 @@ module FormsLab
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
 
-      params[:pirate][:ships].each do |info|
-           Ship.new(info)
-      end
-      @ships = Ship.all
+      # params[:pirate][:ships].each do |info|
+      #      Ship.new(info)
+      # end
+      # @ships = Ship.all
 
       erb :pirates
       binding.pry
